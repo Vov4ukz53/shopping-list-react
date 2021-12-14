5ce4444b72b2c7114abd1c61f7e3271b9cd5df64
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2021-12-14 22:27:51 +0100

    added colors

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,10 @@
 const theme = {
    colors: {
       borderColor: "#e5e2e2",
-      mainButton: "#1283a5",
+      mainColor: "#1283a5",
       firebrick: "#B22222",
+      white: "#fff",
+      linen: "#FAF0E6",
    },
    breakpoints: {
       tablets: 992,
