ed3bffec4a4f7c54fdc0be0a8a35802c2b3010d7
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2021-12-14 22:32:46 +0100

    added more pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
-import Products from "./features/products/Products";
+import { HashRouter, Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
+import Products from "./features/products/ProductsPage";
+import Author from "./features/author/Author";
+import { Redirect } from "react-router-dom";
 
-const App = () => <Products />;
+const App = () => (
+   <HashRouter>
+      <Switch>
+         <Route path="/producty">
+            <Products />
+         </Route>
+         <Route path="/author">
+            <Author />
+         </Route>
+         <Route path="/">
+            <Redirect to="/producty" />
+         </Route>
+      </Switch>
+   </HashRouter>
+);
 
 export default App;
