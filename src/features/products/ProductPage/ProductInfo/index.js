8d9e49b1773e3a1bba06a997cb59e9576d7c23a7
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2021-12-15 22:03:01 +0100

    added new information

--- src/features/products/ProductPage/ProductInfo/index.js
+++ src/features/products/ProductPage/ProductInfo/index.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectGetProductById } from "../../productsSlice";
+import { selectGetProductById, selectProducts } from "../../productsSlice";
 import { Wrapper, Paragraph, Strong } from "./styled";
 
 function ProductInfo() {
    const { id } = useParams();
    const product = useSelector(state => selectGetProductById(state, id));
+   const products = useSelector(selectProducts)
 
    return (
       product
          ?
          <Wrapper>
             <Paragraph>
-               <Strong>Nazwa produktu:</Strong> {product.content}!
+               <Strong>Nazwa produktu:</Strong> {product.content} 
             </Paragraph>
             <Paragraph>
-               <Strong>Product zaznaczony:</Strong> {product.done ? "Tak!" : "Nie!"}
+               <Strong>Product zaznaczony:</Strong> {product.done ? "Tak" : "Nie"}
+            </Paragraph>
+            <Paragraph>
+               <Strong>Kolejnośc na liście:</Strong> {products.indexOf(product) + 1} 
             </Paragraph>
          </Wrapper>
          : <Paragraph notFind>Nie znaleziono produktu!</Paragraph>
