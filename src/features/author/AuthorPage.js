594935c58a187f9fd1c8b08470c4f239126dc258
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2021-12-23 20:36:32 +0100

    added information

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -1,6 +1,8 @@
 import Container from "../../common/Container";
+import { Paragraph, Wrapper, Strong, Div } from "./styled";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
+
 
 function AuthorPage() {
    return (
@@ -10,11 +12,22 @@ function AuthorPage() {
          />
          <Container>
             <Section
-               title="Volodymyr Bronevych"
+               title="Vova Bronevych"
                body={
-                  <p>
-                     Czesc. 
-                  </p>
+                  <Wrapper>
+                     <Div>
+                        <Paragraph>
+                           <Strong>Witam serdecznie!</Strong> Mam 31 lat i pochodzę z Ukrainy, studiowałem we Lwowie.
+                        </Paragraph>
+                        <Paragraph>
+                           Od 2014r mieszkam we Wrocławiu. Mam dwie córeczki, które mnie codziennie motywują i napełniają każdy dzień pozytywem!
+                        </Paragraph>
+                        <Paragraph>
+                           Od roku uczę się frontendu i moim marzeniem jest <Strong>zostać front-end developerem.</Strong> Ponieważ chce robic na co dzień to co mi się podoba.
+                        </Paragraph>
+                     </Div>
+                     <Div image />
+                  </Wrapper>
                }
             />
          </Container>
---
+++ src/features/author/styled.js
@@ -0,0 +1,48 @@
+import styled, { css, keyframes } from "styled-components";
+import frontend from "./img/frontend.png";
+
+export const Wrapper = styled.div`
+   display: grid;
+   grid-template-columns: auto 200px;
+   grid-gap: 15px;
+   align-items: center;
+   padding: 20px;
+   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+      padding: 20px 10px;
+      grid-template-columns: 1fr;
+   }
+`;
+
+export const Paragraph = styled.p`
+   margin: 0px 0px 15px 0px;
+
+   &:last-child{
+      margin: 0;
+   }
+`;
+
+export const Strong = styled.strong`
+   letter-spacing: 1.2px;
+`;
+
+export const Div = styled.div`
+   ${({ image }) => image && css`
+      background-image: url("${frontend}");
+      height: 200px;
+      animation: ${rotate} 60s linear infinite;
+
+      @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+         width: 200px;
+         margin: 0 auto;
+      }
+   `}
+`;
+
+const rotate = keyframes`
+   from {
+      transform: rotate(0deg);
+   }
+   to {
+      transform: rotate(-360deg);
+   }
+`; 
