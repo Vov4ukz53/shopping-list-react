b03e5b3416a1470596cad27e735c0dbdb00380d2
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2021-12-23 20:31:57 +0100

    removed grid gap

--- src/features/products/ProductsPage/Buttons/styled.js
+++ src/features/products/ProductsPage/Buttons/styled.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
    display: grid;
 	grid-template-columns: auto auto;
-	grid-gap: 10px;
 
    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
 	   grid-template-columns: 1fr;
