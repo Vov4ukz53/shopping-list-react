b8cb71d55416bf66a2de2968ece2f916c08a5b4e
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2021-12-31 18:06:10 +0100

    rename rootSaga

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
-import rootSAGA from "./features/products/rootSAGA";
+import rootSaga from "./features/products/rootSaga";
 import productsReducer from "./features/products/productsSlice";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -12,6 +12,6 @@ const store = configureStore({
     middleware: [sagaMiddleware],
 });
 
-sagaMiddleware.run(rootSAGA);
+sagaMiddleware.run(rootSaga);
 
 export default store;
