2b8002d650e4b308ed115e8155e7743eebae2ace
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2021-12-31 13:31:54 +0100

    rename rootSaga

--- src/features/products/rootSAGA.js
+++ src/features/products/rootSAGA.js
@@ -1,7 +1,7 @@
 import { all } from "redux-saga/effects";
-import productsSaga from "./productsSAGA";
+import productsSaga from "./productsSaga";
 
-export default function* rootSAGA() {
+export default function* rootSaga() {
     yield all([
         productsSaga(),
     ]);
